f2a913fe35d1372f9264651e799b1e20ce65a09d
Samir Faci <samirf@wolfram.com>
2011-04-12 10:32:58 -0500

    fix to explicitly specify the LVM name.

--- open.sh
+++ open.sh
@@ -3,10 +3,6 @@ mkdir loopback
 sudo losetup /dev/loop0 foobar.raw
 sudo cryptsetup luksOpen /dev/loop0 cryptoLoop
 sudo vgscan 
-sudo vgchange -a y
-#sudo pvcreate /dev/mapper/cryptoLoop
-#sudo vgcreate cryptoLvmGrp /dev/mapper/cryptoLoop
-#sudo lvcreate -l100%VG -n lvmData cryptoLvmGrp
-#sudo mkfs  -t ext3 /dev/cryptoLvmGrp/lvmData
+sudo vgchange -a y cryptoLvmGrp
 sudo mount /dev/cryptoLvmGrp/lvmData ./loopback
 sudo chmod 777 loopback
--- shutdown.sh
+++ shutdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo umount loopback
-sudo vgchange -a n 
+sudo vgchange -a n  cryptoLvmGrp
 sudo cryptsetup luksClose cryptoLoop
 sudo losetup -d /dev/loop0 && rm -fr loopback/
